193dd13c9ed6af796245e74151df787d299f882c
doityoo <yitsky@naver.com>
2023-01-06 11:01:11 +0900

    feat: class-static 정적 메소드/변수 지정 및 메소드로 사용

--- class/src/class_static.ts
+++ class/src/class_static.ts
@@ -1,6 +1,7 @@
 class Department {
   // private readonly id: string;
   // private name: string;
+  static fiscalYear: 2022;
   protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
@@ -31,4 +32,4 @@ class Department {
 
 // 이런식으로 해당 클래스를 바로 사용할 수 있다.
 const employee1 = Department.createEmployee('Willy');
-console.log(employee1); // {name : 'Willy'}
+console.log(employee1, Department.fiscalYear); // {name : 'Willy'} 2022
