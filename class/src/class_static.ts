23e2c61277939befa4921ded38e24d260aa7b182
doityoo <yitsky@naver.com>
2023-01-06 10:55:05 +0900

    feat: class-static정적 메소드 사용하여 인스턴스로 호출하지 않고 클래스에 직접 접근할 수 있음

---
+++ class/src/class_static.ts
@@ -0,0 +1,34 @@
+class Department {
+  // private readonly id: string;
+  // private name: string;
+  protected employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  // 클래스의 인스턴스를 생성하지 않고 바로 사용할수 있는 정적 메소드
+  static createEmployee(name: string) {
+    return { name: name };
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    // validation etc
+    // this.id = 'd2';
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+// 이런식으로 해당 클래스를 바로 사용할 수 있다.
+const employee1 = Department.createEmployee('Willy');
+console.log(employee1); // {name : 'Willy'}
